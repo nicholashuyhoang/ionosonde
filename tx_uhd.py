8a88ab3306031f54831e9ebb61d51d044a6a6461
Smail6992 <smail@vt.edu>
2024-01-08 11:03:29 -0500

    tx_uhd.py edited

--- tx_uhd.py
+++ tx_uhd.py
@@ -171,10 +171,19 @@ def main(config):
 
     # wait until GPS is locked, then align USRP time with global ref
     
-    #TOD0
+    gps_mon = None
     
-    gl.sync_clock(usrp, log, min_sync_time=ic.min_gps_lock_time)
-    gps_mon=gl.gpsdo_monitor(usrp, log, ic.gps_holdover_time)
+    if ic.require_gps == False:
+        usrp.set_clock_source("external");
+        usrp.set_time_source("external");
+        time_at_last_pps = usrp.get_time_last_pps().get_real_secs()
+        while time_at_last_pps == usrp.get_time_last_pps().get_real_secs():
+            time.sleep(0.1)
+        usrp.set_time_next_pps(uhd.libyuhd.types.tem_spec(int(n.ceil(time.time()))))
+    else:   
+        gl.sync_clock(usrp, log, min_sync_time=ic.min_gps_lock_time)
+        gps_mon=gl.gpsdo_monitor(usrp, log, ic.gps_holdover_time)
+     
 
     # start with first frequency on tx and rx
     tune_req=uhd.libpyuhd.types.tune_request(s.freq(0))
