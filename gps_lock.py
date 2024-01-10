d2013422cbd72eae97039f74cda783933287bdfb
Ehite Anteneh <ehiteanteneh@vt.edu>
2024-01-10 16:47:33 -0500

    updated clock to be external and commented references to mboard

--- gps_lock.py
+++ gps_lock.py
@@ -6,7 +6,10 @@ import stuffr
 
 
 def check_lock(u, log=None, exit_if_not_locked=False):
-    locked=u.get_mboard_sensor("gps_locked").to_bool()
+    #locked=u.get_mboard_sensor("gps_locked").to_bool()
+
+    #commented out anything referrencing ^get_mboard_sensor so now this function will always return true
+    locked = true
 
     with open("gps.log", "a") as f:
         f.write("%s lock=%d\n" % (stuffr.unix2datestr(time.time()), locked))
@@ -19,7 +22,60 @@ def check_lock(u, log=None, exit_if_not_locked=False):
             exit(0)
     return(locked)
 
+## this is the original function which is for reference while tinkering but actual used function is below this implementation
 
+def original_sync_clock(u, log, min_sync_time=300.0):
+    # Based on the specs for the gpsdo, it takes 5 minutes to warm
+    # up after restart
+    t0=time.time()
+
+    not_locked_for_long_enough=True
+#    if min_sync_time < 10:
+#        min_sync_time=20.0
+
+    while not_locked_for_long_enough:
+        time_locked=time.time()-t0
+
+        if time_locked > min_sync_time:
+            # we've been locked for long enough. start the receiver.
+            not_locked_for_long_enough=False
+
+        print("Waiting for GPS lock.\nObtained lock for %1.0f/%1.0f seconds\n"
+              "Check GPS antenna if no lock obtained in 60 seconds." % (time_locked, min_sync_time))
+        time.sleep(10)
+        # check for lock. don't log or exit
+        gps_locked=check_lock(u)
+
+        if not gps_locked:
+            # reset t0 if not locked
+            t0=time.time()
+
+    # originally "gpsdo" but we will be using an external clock
+    u.set_clock_source("gpsdo")
+    u.set_time_source("gpsdo")
+
+    lastt=u.get_time_last_pps()
+    nextt=u.get_time_last_pps()
+    while nextt==lastt:
+        time.sleep(0.05)
+        lastt=nextt
+        nextt=u.get_time_last_pps()
+    time.sleep(0.2)
+    u.set_time_next_pps(uhd.libpyuhd.types.time_spec(u.get_mboard_sensor("gps_time").to_int()+1))
+
+    log.log(str(u.get_mboard_sensor("gps_gpgga")))
+    log.log(str(u.get_mboard_sensor("gps_gprmc")))
+
+    time.sleep(2.0)
+
+    t_now=time.time()
+    t_usrp=(u.get_time_now().get_full_secs()+u.get_time_now().get_frac_secs())
+    t_gpsdo=u.get_mboard_sensor("gps_time")
+    # these should be similar
+    print("pc clock %1.2f usrp clock %1.2f gpsdo %1.2f" % (t_now, t_usrp, t_gpsdo.to_int()))
+
+
+# commented out anything that requires mother board calls
 def sync_clock(u, log, min_sync_time=300.0):
     # Based on the specs for the gpsdo, it takes 5 minutes to warm
     # up after restart
@@ -46,8 +102,9 @@ def sync_clock(u, log, min_sync_time=300.0):
             # reset t0 if not locked
             t0=time.time()
 
-    u.set_clock_source("gpsdo")
-    u.set_time_source("gpsdo")
+    # original:"external" = "gpsdo" but we will be using an external clock
+    u.set_clock_source("external")
+    u.set_time_source("external")
 
     lastt=u.get_time_last_pps()
     nextt=u.get_time_last_pps()
@@ -56,19 +113,27 @@ def sync_clock(u, log, min_sync_time=300.0):
         lastt=nextt
         nextt=u.get_time_last_pps()
     time.sleep(0.2)
-    u.set_time_next_pps(uhd.libpyuhd.types.time_spec(u.get_mboard_sensor("gps_time").to_int()+1))
 
-    log.log(str(u.get_mboard_sensor("gps_gpgga")))
-    log.log(str(u.get_mboard_sensor("gps_gprmc")))
+    # original: u.set_time_next_pps(uhd.libpyuhd.types.time_spec(u.get_mboard_sensor("gps_time").to_int()+1))
+    # original: log.log(str(u.get_mboard_sensor("gps_gpgga")))
+    # original: log.log(str(u.get_mboard_sensor("gps_gprmc")))
+    
+    # debugging purposes
+    t_next = time.time() + 1
+    print("t0 =", t0, "t_next = ", t_next," ", int(t_next))
+
+    # this instead uses host computer clock
+    u.set_time_next_pps(uhd.libpyuhd.types.time_spec(int(t_next)))
 
     time.sleep(2.0)
 
     t_now=time.time()
     t_usrp=(u.get_time_now().get_full_secs()+u.get_time_now().get_frac_secs())
-    t_gpsdo=u.get_mboard_sensor("gps_time")
-    # these should be similar
-    print("pc clock %1.2f usrp clock %1.2f gpsdo %1.2f" % (t_now, t_usrp, t_gpsdo.to_int()))
 
+    # originial: t_gpsdo=u.get_mboard_sensor("gps_time")
+    # these should be similar
+    # originial: print("pc clock %1.2f usrp clock %1.2f gpsdo %1.2f" % (t_now, t_usrp, t_gpsdo.to_int()))
+    print("pc clock %1.2f usrp clock %1.2f gpsdo %1.2f" % (t_now, t_usrp))    
 
 class gpsdo_monitor:
     def __init__(self, u, log, holdover_time=1800.0, exit_on_lost_lock=True):
